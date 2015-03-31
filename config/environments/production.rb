19b8871800679e40f6cca7b1cafb63e8c33640da
lyman wong <lymanwong@gmail.com>
2015-03-31 16:26:18 -0700

    moved bucket out of credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ config.i18n.fallbacks = true
   #Configuring Amazon S3 for Paperclip file uploads
   config.paperclip_defaults = {
     :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
